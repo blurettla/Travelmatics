b78ab9b77dba83c171c3bd1771bb6b55764709b1
blurettla <33299613+blurettla@users.noreply.github.com>
2019-08-04 15:21:13 +0200

    - Added a Save menu - Added code to insert text into the firebase database

--- app/src/main/java/za/co/kerno/travelmatics/InsertActivity.java
+++ app/src/main/java/za/co/kerno/travelmatics/InsertActivity.java
@@ -1,14 +1,76 @@
 package za.co.kerno.travelmatics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class InsertActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_insert);
-    }
-}
+public class InsertActivity extends AppCompatActivity
+	{
+		private FirebaseDatabase mFirebaseDatabase;
+		private DatabaseReference mDatabaseReference;
+		EditText txtTitle;
+		EditText txtDescription;
+		EditText txtPrice;
+		@Override
+		protected void onCreate(Bundle savedInstanceState)
+			{
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.activity_insert);
+				mFirebaseDatabase = FirebaseDatabase.getInstance();
+				mDatabaseReference =mFirebaseDatabase.getReference().child("traveldeals");
+
+				txtTitle = findViewById(R.id.txtTitle);
+				txtDescription = findViewById(R.id.txtDescription);
+				txtPrice = findViewById(R.id.txtPrice);
+			}
+
+		@Override
+		public boolean onOptionsItemSelected(@NonNull MenuItem item)
+			{
+				switch (item.getItemId())
+					{
+						case R.id.save_menu:
+							saveDeal();
+							Toast.makeText(this, "The deal has been saved", Toast.LENGTH_LONG).show();
+							cleanFields();
+							return true;
+						default:
+								return super.onOptionsItemSelected(item);
+					}
+
+			}
+
+		private void cleanFields()
+			{
+				txtTitle.setText("");
+				txtDescription.setText("");
+				txtPrice.setText("");
+				txtTitle.requestFocus();
+			}
+
+		private void saveDeal()
+			{
+				String title = txtTitle.getText().toString();
+				String description= txtDescription.getText().toString();
+				String price = txtPrice.getText().toString();
+				TravelDeal deal = new TravelDeal(title, description, price, "");
+				mDatabaseReference.push().setValue(deal);
+			}
+
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu)
+			{
+				MenuInflater inflater = getMenuInflater();
+				inflater.inflate(R.menu.save_menu, menu);
+				return true;
+			}
+	}
