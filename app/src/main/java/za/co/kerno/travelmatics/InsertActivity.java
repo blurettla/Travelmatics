fa44e02a0b55e33b3e407d959f7b47362b7955bf
blurettla <33299613+blurettla@users.noreply.github.com>
2019-08-04 17:52:19 +0200

    Replaced textviews with a Recyclerview and also added a deal adapter and a firebase utility file

--- app/src/main/java/za/co/kerno/travelmatics/InsertActivity.java
+++ app/src/main/java/za/co/kerno/travelmatics/InsertActivity.java
@@ -25,8 +25,9 @@ public class InsertActivity extends AppCompatActivity
 			{
 				super.onCreate(savedInstanceState);
 				setContentView(R.layout.activity_insert);
-				mFirebaseDatabase = FirebaseDatabase.getInstance();
-				mDatabaseReference =mFirebaseDatabase.getReference().child("traveldeals");
+				FirebaseUtil.openFbReference("traveldeals");
+				mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+				mDatabaseReference = FirebaseUtil.mDatabaseReference;
 
 				txtTitle = findViewById(R.id.txtTitle);
 				txtDescription = findViewById(R.id.txtDescription);
