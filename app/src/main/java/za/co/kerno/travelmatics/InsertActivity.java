91771adb3c5f37ae08a16d783279947d0feace8f
blurettla <33299613+blurettla@users.noreply.github.com>
2019-08-04 09:41:33 +0200

    - Created the insert activity - Added the JSON file to enable authentication - Linked the app to firebase by adding dependencis

---
+++ app/src/main/java/za/co/kerno/travelmatics/InsertActivity.java
@@ -0,0 +1,14 @@
+package za.co.kerno.travelmatics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
